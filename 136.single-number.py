e03200739e8a21bac58a85f5a8f35fb607adaff7
SavviSam <swghazzawi@gmail.com>
2024-10-13 22:04:30 -0600

    solutions

---
+++ 136.single-number.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=136 lang=python3
+#
+# [136] Single Number
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        res = 0
+        for n in nums:
+            res ^= n
+        return res
+
+# @lc code=end
---
+++ 2022.convert-1-d-array-into-2-d-array.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=2022 lang=python3
+#
+# [2022] Convert 1D Array Into 2D Array
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
+        
+
+
+
+        
+# @lc code=end
+
