28d5fb45e1d65c58d4a283b5a289b3842aa0fbd8
SavviSam <swghazzawi@gmail.com>
2024-10-14 00:38:05 -0600

    solutions 10/13

--- 2022.convert-1-d-array-into-2-d-array.py
+++ 2022.convert-1-d-array-into-2-d-array.py
@@ -10,10 +10,7 @@ from typing import List
 
 class Solution:
     def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
-        
+   
 
 
-
-        
 # @lc code=end
-
---
+++ 283.move-zeroes.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=283 lang=python3
+#
+# [283] Move Zeroes
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+
+# @lc code=end
